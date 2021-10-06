655b2f2b4f531856ded8b84b4d40d72ee9a0ad36
Ade Iqbal <adeiqbal1305@gmail.com>
2021-10-07 02:25:20 +0700

    launcher, getstarted

--- app/src/main/java/com/nongskuy/nongskuy/LauncherActivity.java
+++ app/src/main/java/com/nongskuy/nongskuy/LauncherActivity.java
@@ -1,8 +1,11 @@
 package com.nongskuy.nongskuy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(LauncherActivity.this, GetStartedActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
