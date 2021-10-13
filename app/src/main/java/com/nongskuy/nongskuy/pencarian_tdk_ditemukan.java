14810a966cc73fdb37ce3c8195b20b9dbdd8299c
will_wahyu <williamwahyu30@gmail.com>
2021-10-13 23:03:33 +0700

    Pun

---
+++ app/src/main/java/com/nongskuy/nongskuy/pencarian_tdk_ditemukan.java
@@ -0,0 +1,14 @@
+package com.nongskuy.nongskuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pencarian_tdk_ditemukan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pencarian_tdk_ditemukan);
+    }
+}
