d0550a76c84fd30575bc77d706939da1f8441d6f
Latifatuzikra Suhairi <latifatuzikra@gmail.com>
2021-10-13 13:19:02 +0700

    Profil

--- app/src/main/java/com/nongskuy/nongskuy/PromoFragment.java
+++ app/src/main/java/com/nongskuy/nongskuy/PromoFragment.java
@@ -15,8 +15,6 @@ public class PromoFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
