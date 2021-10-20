353d284ac59d1cb3b417ce200c71aef408292873
Latifatuzikra Suhairi <latifatuzikrasuhairi@users.noreply.github.com>
2021-10-20 22:42:38 +0700

    XML Riwayat Nongskuy

--- app/src/main/java/com/nongskuy/nongskuy/BerandaFragment.java
+++ app/src/main/java/com/nongskuy/nongskuy/BerandaFragment.java
@@ -1,5 +1,7 @@
 package com.nongskuy.nongskuy;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,17 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class BerandaFragment extends Fragment {
 
     RecyclerView recyclerViewBerandaPopuler;
     TextView namaUser;
+    MaterialButton btnRiwayatPemesananTempat;
 
     public BerandaFragment() {
         // Required empty public constructor
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class BerandaFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_beranda, container, false);
 
+        btnRiwayatPemesananTempat = view.findViewById(R.id.buttonPesanTempat);
 
         //Mengambil email login user
         namaUser = view.findViewById(R.id.textName);
@@ -35,6 +41,11 @@ public class BerandaFragment extends Fragment {
             namaUser.setText("William Wahyu");
         }
 
+        //Intent Fragment Beranda ke Activity Riwayat Pesan Tempat
+        btnRiwayatPemesananTempat.setOnClickListener(view1 -> {
+            Intent intent = new Intent(getActivity(), RiwayatPemesananTempatActivity.class);
+            startActivity(intent);
+        });
 
         return view;
     }
---
+++ app/src/main/java/com/nongskuy/nongskuy/RiwayatPemesananTempatActivity.java
@@ -0,0 +1,14 @@
+package com.nongskuy.nongskuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiwayatPemesananTempatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riwayat_pemesanan_tempat);
+    }
+}
