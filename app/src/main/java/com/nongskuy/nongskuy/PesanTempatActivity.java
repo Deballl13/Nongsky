b507c56259d1fc44bb0bcea2c0fbd15d472c22ac
Latifatuzikra Suhairi <latifatuzikrasuhairi@users.noreply.github.com>
2021-11-05 20:46:34 +0700

    DatePicker n TimePicker

--- app/src/main/java/com/nongskuy/nongskuy/PesanTempatActivity.java
+++ app/src/main/java/com/nongskuy/nongskuy/PesanTempatActivity.java
@@ -3,62 +3,80 @@ package com.nongskuy.nongskuy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.android.material.textfield.TextInputLayout;
-
+import android.widget.TimePicker;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class PesanTempatActivity extends AppCompatActivity {
-    EditText Tanggal;
-    TextView textTanggal;
-//    DatePickerDialog.OnDateSetListener setListener;
+
+    EditText Tanggal, Waktu;
+    Calendar calendar;
+    int Hour, Minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pesan_tempat);
 
-        textTanggal = findViewById(R.id.textJdlIsi);
         Tanggal = findViewById(R.id.Tanggal);
+        Waktu = findViewById(R.id.Waktu);
 
-//        Calendar calendar = Calendar.getInstance();
-//        final int year = calendar.get(Calendar.YEAR);
-//        final int month = calendar.get(Calendar.MONTH);
-//        final int day = calendar.get(Calendar.DAY_OF_MONTH);
-//
-//        textTanggal.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DatePickerDialog datePickerDialog = new DatePickerDialog(
-//                        PesanTempatActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,
-//                        setListener, year, month, day);
-//                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-//                datePickerDialog.show();
-//            }
-//        });
-//
-//        Tanggal.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DatePickerDialog datePickerDialog = new DatePickerDialog(
-//                        PesanTempatActivity.this, new DatePickerDialog.OnDateSetListener() {
-//                    @Override
-//                    public void onDateSet(DatePicker view, int year, int month, int day) {
-//                        month = month+1;
-//                        String date = day+"/"+month+"/"+year;
-//                        Tanggal.setText(date);
-//                    }
-//                }
-//                )
-//            }
-//        });
-//
+        calendar = Calendar.getInstance();
+
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                calendar.set(calendar.YEAR, year);
+                calendar.set(calendar.MONTH, month);
+                calendar.set(calendar.DAY_OF_MONTH, day);
+
+                updateCalendar();
+            }
+
+            private void updateCalendar() {
+                String formatTanggal = "dd/MM/yyyy";
+                SimpleDateFormat dateformat = new SimpleDateFormat(formatTanggal, Locale.US);
+
+                Tanggal.setText(dateformat.format(calendar.getTime()));
+            }
+        };
+
+        Tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(PesanTempatActivity.this, date, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        Waktu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(PesanTempatActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        //Inisialisasi waktu
+                        Hour = hourOfDay;
+                        Minute = minute;
+
+                        //Set hour and minute
+                        calendar.set(0,0,0, Hour, Minute);
+                        //Set selected time
+                        String formatWaktu = "hh:mm aa";
+                        SimpleDateFormat timeformat = new SimpleDateFormat(formatWaktu, Locale.US);
+                        Waktu.setText(timeformat.format(calendar.getTime()));
+                    }
+                }, 24, 0, true);
+                //Display
+                timePickerDialog.updateTime(Hour, Minute);
+                timePickerDialog.show();
+            }
+        });
     }
 }
