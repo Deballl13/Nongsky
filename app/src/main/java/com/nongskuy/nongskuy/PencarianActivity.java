5d145fbbbb1e43bae10d711b061685fec6addc47
Latifatuzikra Suhairi <latifatuzikrasuhairi@users.noreply.github.com>
2021-11-28 20:24:51 +0700

    Perbaikan rvPencarian

--- app/src/main/java/com/nongskuy/nongskuy/PencarianActivity.java
+++ app/src/main/java/com/nongskuy/nongskuy/PencarianActivity.java
@@ -1,14 +1,102 @@
 package com.nongskuy.nongskuy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.nongskuy.nongskuy.adapter.PencarianAdapter;
+import com.nongskuy.nongskuy.adapter.RiwayatNongskuyAdapter;
+import com.nongskuy.nongskuy.model.RiwayatNongskuy;
+import com.nongskuy.nongskuy.model.Store;
+
+import java.util.ArrayList;
 
 public class PencarianActivity extends AppCompatActivity {
+
+    RecyclerView rvPencarian;
+    PencarianAdapter pencarianAdapter;
+    ConstraintLayout layoutPencarianDitemukan;
+    ConstraintLayout layoutPencarianTidakDitemukan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pencarian);
+
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
+
+        layoutPencarianDitemukan = findViewById(R.id.layoutPencarianDitemukan);
+        layoutPencarianTidakDitemukan = findViewById(R.id.layoutPencarianTidakDitemukan);
+
+        pencarianAdapter = new PencarianAdapter();
+        pencarianAdapter.setListPencarian(dataDummy());
+
+        if(pencarianAdapter.getItemCount() > 0){
+            rvPencarian = findViewById(R.id.rvPencarian);
+            rvPencarian.setAdapter(pencarianAdapter);
+            rvPencarian.setLayoutManager(layoutManager);
+
+            layoutPencarianDitemukan.setVisibility(View.VISIBLE);
+            layoutPencarianTidakDitemukan.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public ArrayList<Store> dataDummy(){
+        ArrayList<Store> listPencarian = new ArrayList<>();
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+
+        listPencarian.add(new Store(
+                4.2,
+                "McDonald’s Padang",
+                "Jl. Khatib Sulaeman",
+                "Cepat saji",
+                4.5,
+                "km"
+        ));
+        return listPencarian;
     }
 }
--- app/src/main/java/com/nongskuy/nongskuy/adapter/PencarianAdapter.java
+++ app/src/main/java/com/nongskuy/nongskuy/adapter/PencarianAdapter.java
@@ -9,54 +9,58 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.imageview.ShapeableImageView;
+import com.nongskuy.nongskuy.Helper;
 import com.nongskuy.nongskuy.R;
+import com.nongskuy.nongskuy.model.RiwayatNongskuy;
 import com.nongskuy.nongskuy.model.Store;
 
 import java.util.ArrayList;
 
 public class PencarianAdapter extends RecyclerView.Adapter<PencarianAdapter.PencarianViewHolder>{
 
-    private ArrayList<Store> listPencarian;
-
-    public PencarianAdapter(ArrayList<Store> listPencarian) {
-        this.listPencarian = listPencarian;
-    }
-
-    @NonNull
-    @Override
-    public PencarianAdapter.PencarianViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.card_pencarian, parent, false);
-
-        return new PencarianAdapter.PencarianViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull PencarianAdapter.PencarianViewHolder holder, int position) {
-//        holder.textNamaTokoPencarian.setText(listPencarian.get(position).getRating().toString());
-//        holder.textAlamatTokoPencarian.setText(listPencarian.get(position).getNamaToko());
-//        holder.textJarakToko.setText(listPencarian.get(position).getTipeToko());
-//        holder.logoTokoPencarian.setImageResource(listPencarian.get(position).getJarakToko().toString());
-//        holder.satuanJarak.setText(listPencarian.get(position).getSatuanJarak());
-    }
-
-    @Override
-    public int getItemCount() {
-        return listPencarian.size();
-    }
-
+    //View Holder
     public class PencarianViewHolder extends RecyclerView.ViewHolder {
         TextView textNamaTokoPencarian, textAlamatTokoPencarian, textJarakToko, satuanJarak;
-        ShapeableImageView logoTokoPencarian;
 
         public PencarianViewHolder(@NonNull View itemView) {
             super(itemView);
             textNamaTokoPencarian = itemView.findViewById(R.id.textNamaTokoPencarian);
             textAlamatTokoPencarian = itemView.findViewById(R.id.textAlamatTokoPencarian);
             textJarakToko = itemView.findViewById(R.id.textJarakToko);
-            logoTokoPencarian = itemView.findViewById(R.id.logoTokoPencarian);
-            satuanJarak = itemView.findViewById(R.id.textSatuanJarakPopulerBeranda);
+            satuanJarak = itemView.findViewById(R.id.satuanJarak);
         }
+    }
+
+    //Array List menampung dan set data yang ditampilkan
+    ArrayList<Store> listPencarian = new ArrayList<>();
+    public void setListPencarian (ArrayList<Store> listPencarian) {
+        this.listPencarian = listPencarian;
+        notifyDataSetChanged();
+    }
+
+    //Method adapter
+    @NonNull
+    @Override
+    public PencarianAdapter.PencarianViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.card_pencarian, parent,false);
+        PencarianAdapter.PencarianViewHolder viewHolder = new PencarianAdapter.PencarianViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PencarianAdapter.PencarianViewHolder viewHolder, int position) {
+
+        Store store = listPencarian.get(position);
+        viewHolder.textNamaTokoPencarian.setText(store.getNamaToko());
+        viewHolder.textAlamatTokoPencarian.setText(store.getAlamatToko());
+        viewHolder.textJarakToko.setText(store.getJarakToko().toString());
+        viewHolder.satuanJarak.setText(store.getSatuanJarak());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listPencarian.size();
     }
 
 }
