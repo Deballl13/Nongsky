b1b9a2c7057dd87155f4e79b31c8c884177196cc
Ade Iqbal <adeiqbal1305@gmail.com>
2021-12-16 19:26:01 +0700

    recyclerview menu promo dan effect shimmer

--- app/src/main/java/com/nongskuy/nongskuy/PromoFragment.java
+++ app/src/main/java/com/nongskuy/nongskuy/PromoFragment.java
@@ -7,13 +7,28 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.nongskuy.nongskuy.adapter.BerandaPromoAdapter;
+import com.nongskuy.nongskuy.adapter.PromoAdapter;
+import com.nongskuy.nongskuy.data.PromoData;
+import com.nongskuy.nongskuy.model.Promo;
+import com.nongskuy.nongskuy.model.PromoClass;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class PromoFragment extends Fragment {
@@ -22,6 +37,8 @@ public class PromoFragment extends Fragment {
     private Integer[] sampleImages = {R.drawable.gado, R.drawable.nuget, R.drawable.pempek, R.drawable.rempah, R.drawable.sushi};
     private RecyclerView recyclerView;
     private SharedPreferences sharedPreferences;
+    private SwipeRefreshLayout refreshLayout;
+    private Config config;
 
     public PromoFragment() {
         // Required empty public constructor
@@ -36,18 +53,36 @@ public class PromoFragment extends Fragment {
         sharedPreferences = getActivity().getSharedPreferences("com.nongskuy.nongskuy.PREFS", Context.MODE_PRIVATE);
         String token = sharedPreferences.getString("Token", null);
 
+        config = new Config();
         carouselView = view.findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
 
-        recyclerView = view.findViewById(R.id.rvPromo);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        recyclerView.setAdapter(new PromoAdapter(getData()));
 
+
+        // cek login atau tidak
         if(token != null){
             view.findViewById(R.id.layoutPromoUser).setVisibility(view.VISIBLE);
             view.findViewById(R.id.layoutPromoGuest).setVisibility(view.GONE);
         }
+
+
+
+        // refresh halaman
+        refreshLayout = view.findViewById(R.id.refreshPromo);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshPage(token);
+            }
+        });
+
+
+
+        // recyclerview promo
+        recyclerView = view.findViewById(R.id.rvPromo);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        loadDataPromo(token);
 
         return view;
     }
@@ -59,79 +94,49 @@ public class PromoFragment extends Fragment {
         }
     };
 
-//    public ArrayList<Promo> getData(){
-//        ArrayList<Promo> listPromo = new ArrayList<>();
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//        listPromo.add(new Promo(
-//                "Sate Taichan",
-//                "Taichan Mimi Peri",
-//                25000,
-//                30000,
-//                "Diskon 5%"
-//        ));
-//
-//        return listPromo;
-//    }
+    public void refreshPage(String token){
+        loadDataPromo(token);
+        refreshLayout.setRefreshing(false);
+    }
+
+    public void loadDataPromo(String token){
+        recyclerView.setAdapter(new PromoAdapter(null));
+        Call<PromoClass> call = config.configRetrofit().promo(token);
+        call.enqueue(new Callback<PromoClass>() {
+            @Override
+            public void onResponse(Call<PromoClass> call, Response<PromoClass> response) {
+                if(response.code() == 200){
+                    if (response.isSuccessful()){
+                        PromoClass promoClass = response.body();
+                        List<PromoData> listPromo = promoClass.getPromo();
+                        ArrayList<Promo> arrayListPromo = new ArrayList<>();
+
+                        for (PromoData promoData: listPromo) {
+                            Promo promo = new Promo(
+                                    promoData.getNamaToko(),
+                                    promoData.getNamaMenu(),
+                                    promoData.getHarga(),
+                                    promoData.getGambar(),
+                                    promoData.getPersentase(),
+                                    promoData.getJenisPromo()
+                            );
+
+                            PromoAdapter recyclerViewPromoAdaper = new PromoAdapter(arrayListPromo);
+                            arrayListPromo.add(promo);
+                            recyclerViewPromoAdaper.setShimmer(false);
+                            recyclerView.setAdapter(recyclerViewPromoAdaper);
+                            recyclerViewPromoAdaper.notifyDataSetChanged();
+                        }
+
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PromoClass> call, Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/nongskuy/nongskuy/adapter/PromoAdapter.java
+++ app/src/main/java/com/nongskuy/nongskuy/adapter/PromoAdapter.java
@@ -1,12 +1,22 @@
 package com.nongskuy.nongskuy.adapter;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.nongskuy.nongskuy.Helper;
 import com.nongskuy.nongskuy.R;
 import com.nongskuy.nongskuy.model.Promo;
@@ -15,6 +25,13 @@ import java.util.ArrayList;
 public class PromoAdapter extends RecyclerView.Adapter<PromoAdapter.PromoViewHolder> {
 
     private ArrayList<Promo> listPromo;
+    private Context context;
+    private boolean isShimmer = true;
+    Integer numberShimmer = 10;
+
+    public void setShimmer(boolean shimmer) {
+        isShimmer = shimmer;
+    }
 
     public PromoAdapter(ArrayList<Promo> listPromo) {
         this.listPromo = listPromo;
@@ -25,34 +42,77 @@ public class PromoAdapter extends RecyclerView.Adapter<PromoAdapter.PromoViewHol
     public PromoAdapter.PromoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.card_promo, parent, false);
-
+        context = view.getContext();
         return new PromoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PromoAdapter.PromoViewHolder holder, int position) {
-        // format mata uang rupiah
-        Helper helper = new Helper();
-        Promo promo = listPromo.get(position);
-        holder.textMenuPromo.setText(promo.getNamaMenu());
-        holder.textTokoPromo.setText(listPromo.get(position).getNamaToko());
-        holder.textHargaSebelumPromo.setText(helper.mataUangRupiah(listPromo.get(position).getHargaAwal()));
+        if(isShimmer){
+            holder.shimmerFrameLayout.startShimmer();
+        }
+        else{
+            holder.shimmerFrameLayout.stopShimmer();
+            holder.shimmerFrameLayout.setShimmer(null);
 
-        holder.textHargaSebelumPromo.setPaintFlags(holder.textHargaSebelumPromo.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            Promo promo = listPromo.get(position);
+
+            // format mata uang rupiah
+            Helper helper = new Helper();
+
+            holder.imagePromo.setBackground(null);
+            Glide.with(context)
+                    .load(Uri.parse(promo.getGambar()))
+                    .apply(new RequestOptions()
+                            .override(82, 84))
+                    .into(holder.imagePromo);
+
+            holder.textMenuPromo.setBackground(null);
+            holder.textMenuPromo.setText(promo.getNamaMenu());
+
+            holder.textTokoPromo.setBackground(null);
+            holder.textTokoPromo.setText(promo.getNamaToko());
+
+            holder.textHargaSebelumPromo.setBackground(null);
+            holder.textHargaSebelumPromo.setText(helper.mataUangRupiah(promo.getHargaAwal()));
+            holder.textHargaSebelumPromo.setPaintFlags(holder.textHargaSebelumPromo.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+            holder.textHargaSetelahPromo.setBackground(null);
+            holder.textHargaSetelahPromo.setText(
+                    helper.mataUangRupiah(promo.getHargaAwal() - ((promo.getPersentase() / 100) * promo.getHargaAwal()))
+            );
+
+            holder.keterangan.setBackground(null);
+            holder.keterangan.setText(promo.getJenis_promo() + " " + promo.getPersentase().toString() + "%");
+
+            if(promo.getJenis_promo().equals("diskon")){
+                holder.keterangan.setTextColor(ContextCompat.getColor(context, R.color.dark_gray));
+                holder.keterangan.setBackgroundResource(R.drawable.background_ket_diskon);
+
+            }
+            else if(promo.getJenis_promo().equals("cashback")){
+                holder.keterangan.setTextColor(Color.WHITE);
+                holder.keterangan.setBackgroundResource(R.drawable.background_ket_cashback);
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-        return listPromo.size();
+        return isShimmer?numberShimmer:listPromo.size();
     }
 
     public class PromoViewHolder extends RecyclerView.ViewHolder{
 
         TextView textMenuPromo, textTokoPromo, textHargaSetelahPromo, textHargaSebelumPromo, keterangan;
+        ShapeableImageView imagePromo;
+        ShimmerFrameLayout shimmerFrameLayout;
 
         public PromoViewHolder(@NonNull View view){
             super(view);
-            textMenuPromo = view.findViewById(R.id.textMakananPromo);
+            shimmerFrameLayout = view.findViewById(R.id.shimmerPromo);
+            imagePromo = view.findViewById(R.id.imagePromo);
+            textMenuPromo = view.findViewById(R.id.textMenuPromo);
             textTokoPromo = view.findViewById(R.id.textTokoPromo);
             textHargaSetelahPromo = view.findViewById(R.id.textHargaSetelahPromo);
             textHargaSebelumPromo = view.findViewById(R.id.textHargaSebelumPromo);
