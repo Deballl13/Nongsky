9eb76f59db59cf4ee35bb3d669b65e7591a73060
Ade Iqbal <adeiqbal1305@gmail.com>
2021-12-28 18:38:12 +0700

    request permission location

--- app/src/main/java/com/nongskuy/nongskuy/TerdekatFragment.java
+++ app/src/main/java/com/nongskuy/nongskuy/TerdekatFragment.java
@@ -1,22 +1,48 @@
 package com.nongskuy.nongskuy;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.SuccessContinuation;
+import com.google.android.gms.tasks.Task;
 
 public class TerdekatFragment extends Fragment {
 
-    private SupportMapFragment supportMapFragment;
+    //    private SupportMapFragment supportMapFragment;
     private SearchView searchView;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location currentLocation;
 
 
-       public TerdekatFragment() {
+    public TerdekatFragment() {
         // Required empty public constructor
     }
 
@@ -26,32 +52,25 @@ public class TerdekatFragment extends Fragment {
         //Initialize view
         View view = inflater.inflate(R.layout.fragment_terdekat, container, false);
 
-        //Initialize view search
-//        searchView = find
-        //Initialize Map Fragment
-        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 404);
+        }
 
-        //Async Map
-        supportMapFragment.getMapAsync(googleMap -> {
-            //When map is loaded
-            googleMap.setOnMapClickListener(latLng -> {
-                //when clicked on map
-                //initialized marker
-                MarkerOptions markerOptions = new MarkerOptions();
-                //set marker position
-                markerOptions.position(latLng);
-                //set Title marker
-                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
-                //remove all marker
-                googleMap.clear();
-                //Animating to zoom marker
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                        latLng, 8
-                ));
-            //Add marker on map
-            googleMap.addMarker(markerOptions);
-            });
-        });
         return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 404){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                getCurrentLocation();
+                Toast.makeText(getActivity(), "berhasil", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
